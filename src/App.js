37fcbd57308ff69c96e4cc0b478b3b46486e1dae
arri <ewws@mail.ru>
2020-12-25 14:27:46 +0900

    fix lera thinks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css'
 import React from 'react'
 import Router from './pages/router'
 
--- src/pages/router.js
+++ src/pages/router.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './login'
+import Sellers from './sellers'
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path={'/login'} component={Login}/>
+                <Route path={'/sellers'} component={Sellers}/>
             </Switch>
         </BrowserRouter>
     )
